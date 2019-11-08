487b812320ee7757e20f2931caa41cef809b663c
Mattias Hising <hising@gmail.com>
2019-11-08 19:45:48 +0100

    Move blogfunc to cms

--- src/core/cms.js
+++ src/core/cms.js
@@ -1,4 +1,4 @@
-import {isAbsolute, setAttributes, trackPageView} from './utils';
+import {fetchFeed, isAbsolute, setAttributes, trackPageView} from './utils';
 import {triggerEvent, on} from './events';
 
 let APP_ROUTES = [];
@@ -20,6 +20,21 @@ export const setAppTitle = (title) => {
 
 const parseForImages = (page) => {
     return page;
+};
+
+export const loadBlogIndex = async (feedURL) => {
+    let feed = await fetchFeed(feedURL);
+    let links = feed.map((item) => {
+        return `<li><a href="${item.link}">${item.title}</a> <small>(${item.published})</small></li>`;
+    });
+    let lead = `<p class="lead">Read posts on our blog on Software Development, Game Development, Analytics, Metrics, User Experience, User Engagement, Onboarding and other pain points in the product development lifecycle</p>`;
+    let subheader = '<h3>Blog Posts</h3>';
+    let footer = '<p>You can find our blog here: <a href="https://yetric.net">yetric.net</a></p>';
+    let content = `<h1>Blog Posts from <em>Yetric</em></h1>${lead}${subheader}<ul>${links.join(
+        ''
+    )}</ul>${footer}`;
+    setAppContent(content);
+    setAppTitle('Blog Posts from Yetric Blog');
 };
 
 export const loadPage = async (tpl, type = 'pages') => {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import './style.scss';
-import {loadPage, nav, setAppContent, setAppTitle} from './core/cms';
-import {fetchFeed, trackPageView} from './core/utils';
+import {loadBlogIndex, loadPage, nav} from './core/cms';
+import {trackPageView} from './core/utils';
 const pageView = async (name) => loadPage(name);
 const postView = async (name) => loadPage(name, 'blog');
 const homeView = async () => loadPage('home');
-
-const blogIndex = async () => {
-    let feed = await fetchFeed('https://yetric.net/rss.xml');
-    let links = feed.map((item) => {
-        return `<li><a href="${item.link}">${item.title}</a> <small>(${item.published})</small></li>`;
-    });
-    let lead = `<p class="lead">Read posts on our blog on Software Development, Game Development, Analytics, Metrics, User Experience, User Engagement, Onboarding and other pain points in the product development lifecycle</p>`;
-    let subheader = '<h3>Blog Posts</h3>';
-    let footer = '<p>You can find our blog here: <a href="https://yetric.net">yetric.net</a></p>';
-    let content = `<h1>Blog Posts from <em>Yetric</em></h1>${lead}${subheader}<ul>${links.join(
-        ''
-    )}</ul>${footer}`;
-    setAppContent(content);
-    setAppTitle('Blog Posts from Yetric Blog');
-};
+const blogIndex = async () => loadBlogIndex('https://yetric.net/rss.xml');
 
 (async () => {
     const cms = await nav({
